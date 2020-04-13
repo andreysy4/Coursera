dfd2f64cbff66c92f9e8ccc1cfa2efbed1053d34
andreySy4 <andreysy4@gmail.com>
2020-04-13 15:05:49 +0300

    Задание 2: Работа с input color

--- Chapter_1/Week_3/main.js
+++ Chapter_1/Week_3/main.js
@@ -8,15 +8,14 @@ function myFun() {
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    return '#'+(Math.random()*0xFFFFFF<<0).toString(16);;
   }
 
 function changeColor() {
     document.getElementById("z2").style.color = getRandomColor();
     document.getElementById("z2").style.backgroundColor = getRandomColor();
+}
+
+function setNewColor(elem) {
+    document.getElementById("z2").style.backgroundColor = elem.value;
 }
