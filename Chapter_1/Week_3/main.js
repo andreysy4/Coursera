310fd6694ffb389257804f58b622c3f4c6c107d4
andreySy4 <andreysy4@gmail.com>
2020-04-13 14:53:52 +0300

    Задание 2: Изменение стиля и содержимого по событию

--- Chapter_1/Week_3/main.js
+++ Chapter_1/Week_3/main.js
@@ -6,3 +6,17 @@ function myFun() {
         document.getElementById("p1").innerHTML = result;
     }
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function changeColor() {
+    document.getElementById("z2").style.color = getRandomColor();
+    document.getElementById("z2").style.backgroundColor = getRandomColor();
+}
