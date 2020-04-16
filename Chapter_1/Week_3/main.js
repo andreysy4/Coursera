ad93db42a4b3e877132313cbff5416baf1c97ec1
andreySy4 <andreysy4@gmail.com>
2020-04-16 15:15:47 +0300

    Задание 3: Convas

--- Chapter_1/Week_3/main.js
+++ Chapter_1/Week_3/main.js
@@ -8,8 +8,8 @@ function myFun() {
 }
 
 function getRandomColor() {
-    return '#'+(Math.random()*0xFFFFFF<<0).toString(16);;
-  }
+    return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);;
+}
 
 function changeColor() {
     document.getElementById("z2").style.color = getRandomColor();
@@ -18,4 +18,47 @@ function changeColor() {
 
 function setNewColor(elem) {
     document.getElementById("z2").style.backgroundColor = elem.value;
+}
+
+function setCanvasColor(elem) {
+    var c = document.getElementById("canId");
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = elem.value;
+    ctx.fillRect(0, 0, c.width, c.height);
+}
+
+function figurki() {
+    var c = document.getElementById("canId");
+    var ctx = c.getContext("2d");
+
+    // Квадрат с инверсией цвета
+    var imgData = ctx.getImageData(0, 0, 1, 1);
+    color = "rgb(0,0,0)";
+    color_standart = "rgb(255,255,255)";
+    if (imgData.data[0]) {
+        color = "rgb(" + (255 - imgData.data[0]) + "," + (255 - imgData.data[1]) + "," + (255 - imgData.data[2]) + ")";
+        color_standart = "rgb(" + imgData.data[0] + "," + imgData.data[1] + "," + imgData.data[2] + ")";
+    }
+    ctx.fillStyle = color;
+    ctx.fillRect(2, 2, 40, 40);
+
+    // Круг 
+    ctx.beginPath();
+    ctx.arc(70, 22, 20, 0, 2 * Math.PI);
+    ctx.fill();
+
+    // Треугольник с градиентом
+    ctx.beginPath();
+    ctx.moveTo(10, 100);
+    ctx.lineTo(10, 190);
+    ctx.lineTo(190, 190);
+    ctx.closePath();
+
+    var grd = ctx.createRadialGradient(100, 150, 5, 100, 150, 100);
+    grd.addColorStop(0, color);
+    grd.addColorStop(1, color_standart);
+    ctx.fillStyle = grd;
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.stroke();
 }
